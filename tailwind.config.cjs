949e968aa792253c1053d8fb957bee448f2c8a42
Saransh Saini <saranshsss99@gmail.com>
2022-10-16 10:16:46 +0530

    project setup and tailwind setup completed

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*{.jsx,js,tsx,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        "indigo-dye": "124E78",
+        "orange-yellow": "F2BB05",
+        "burnt-orange": "D74E09",
+      },
+    },
+  },
+  plugins: [],
+};
