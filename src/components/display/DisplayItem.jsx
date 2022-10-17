fc35802b29fdd997f529c15fe47c78598fa12d04
Saransh Saini <saranshsss99@gmail.com>
2022-10-17 11:57:28 +0530

    border changed,made it responsive

--- src/components/display/DisplayItem.jsx
+++ src/components/display/DisplayItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const DisplayItem = ({ item }) => {
   return (
-    <div className=" bg-lime-400 border-2 py-4 flex flex-col items-center gap-2">
+    <div className="border border-black py-4 flex flex-col items-center gap-2">
       <p className="text-3xl">{item.emoji}</p>
       <p className="text-center">{item.group}</p>
     </div>
--- src/components/display/index.jsx
+++ src/components/display/index.jsx
@@ -7,10 +7,9 @@ const DisplayContainer = () => {
   for (let i = 0; i < 70; i++) {
     tempData.push(data[Math.floor(Math.random() * data.length)]);
   }
-  console.log(tempData);
   return (
     <>
-      <div className=" grid grid-cols-10">
+      <div className=" grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-10">
         {tempData.map((item, index) => {
           return <DisplayItem key={index} item={item} />;
         })}
