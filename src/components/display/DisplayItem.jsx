6380d19b069a8245f412a224adef1fc7150db490
Saransh Saini <saranshsss99@gmail.com>
2022-10-17 11:27:27 +0530

    random emoji items added

--- src/components/display/DisplayItem.jsx
+++ src/components/display/DisplayItem.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-const DisplayItem = () => {
-  return <div> DisplayItem</div>;
+const DisplayItem = ({ item }) => {
+  return (
+    <div className=" bg-lime-400 border-2 py-4 flex flex-col items-center gap-2">
+      <p className="text-3xl">{item.emoji}</p>
+      <p className="text-center">{item.group}</p>
+    </div>
+  );
 };
 
 export default DisplayItem;
--- src/components/display/index.jsx
+++ src/components/display/index.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import DisplayItem from "./DisplayItem";
+import { data } from "/src/data/data.js";
 
 const DisplayContainer = () => {
+  const tempData = [];
+  for (let i = 0; i < 70; i++) {
+    tempData.push(data[Math.floor(Math.random() * data.length)]);
+  }
+  console.log(tempData);
   return (
-    <div>
-      <DisplayItem />
-    </div>
+    <>
+      <div className=" grid grid-cols-10">
+        {tempData.map((item, index) => {
+          return <DisplayItem key={index} item={item} />;
+        })}
+      </div>
+    </>
   );
 };
 
