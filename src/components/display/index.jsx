0cfb52d0d6c2e28434772546938169be49184fad
Saransh Saini <saranshsss99@gmail.com>
2022-10-17 12:31:43 +0530

    state created for data

--- src/components/display/index.jsx
+++ src/components/display/index.jsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import DisplayItem from "./DisplayItem";
 import { data } from "/src/data/data.js";
 
-const DisplayContainer = () => {
+const generateRandom = () => {
   const tempData = [];
   for (let i = 0; i < 70; i++) {
     tempData.push(data[Math.floor(Math.random() * data.length)]);
   }
+  return tempData;
+};
+const DisplayContainer = () => {
+  const [currentData, setCurrentData] = useState(generateRandom());
+  useEffect(() => {
+    setInterval(() => {
+      setCurrentData(generateRandom());
+    }, 10000);
+  }, []);
+
   return (
     <>
-      <div className=" grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-10">
-        {tempData.map((item, index) => {
+      <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-10 p-5">
+        {currentData.map((item, index) => {
           return <DisplayItem key={index} item={item} />;
         })}
       </div>
